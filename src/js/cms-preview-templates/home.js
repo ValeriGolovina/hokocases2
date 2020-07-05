132f035b77ca2c64faa9991cef8006faa36a609a
Valeriia Holovina <Valeriia_Holovina@epam.com>
2020-07-05 12:49:07 +0300

    Changed visuals

--- src/js/cms-preview-templates/home.js
+++ src/js/cms-preview-templates/home.js
@@ -11,18 +11,18 @@ export const HomePreview = ({entry, getAsset, widgetFor}) => {
         image = window.parent.location.protocol + "//" + window.parent.location.host + image;
     }
     return (
-        <div className="pv5 pv6-l ph3 bg-center cover" style={{
+        <div className="ph3 cover" style={{
             backgroundImage: image && `url(${image})`
           }}>
-            <div className="mw7 center ph3">
-            <div className="db mb3">
+            <div className="mw7 center ph3 pv3">
+            <div className="db">
                 <div className="mw7 relative bg-fix-primary mb3">
-                <h1 className="f2 f1-l b di lh-title mb3 white mw6 bg-primary">
+                <h1 className="f2 f1-l b di lh-title mb3 white mw6 bg-primary center">
                     { entry.getIn(["data", "title"]) }
                 </h1>
                 </div>
                 <div className="mw7 relative bg-fix-primary">
-                {<p className="b f4 di lh-title mb3 white mw6 bg-primary">{ entry.getIn(["data", "subtitle"]) }</p>}
+                <p className="b f4 di lh-title mb3 white mw6 center">{ entry.getIn(["data", "subtitle"]) }</p>
                 </div>
             </div>
                 <div className="center mw6 pv3">
